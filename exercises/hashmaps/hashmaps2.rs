6377924c7e8e700666bb68ddbc91e9380c81d8f9
yyheroi <crystalyyhero@gmail.com>
2025-04-10 15:42:37 +0800

    update hashmap

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -40,7 +40,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if fruit != basket.entry(word).or_insert(0)
+            break
+        match basket.get(fruit) {
+            Some(hav) => break,
+            _ => basket.insert(fruit, 1),
+        }
     }
+
 }
 
 #[cfg(test)]
